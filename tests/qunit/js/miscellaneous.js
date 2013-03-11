824a8ac56a26ab4eb1a7d77bb36773a74e81bcb2
Jonathan Olson <olsonsjc@gmail.com>
2013-03-11 14:46:32 -0700

    Working unit tests

---
+++ tests/qunit/js/miscellaneous.js
@@ -0,0 +1,37 @@
+
+(function(){
+  'use strict';
+  
+  module( 'Kite: Shapes' );
+  
+  var Shape = kite.Shape;
+  
+  function p( x, y ) { return new dot.Vector2( x, y ); }
+  
+  test( 'Line segment winding', function() {
+    var line = new kite.Segment.Line( p( 0, 0 ), p( 2, 2 ) );
+    
+    equal( line.windingIntersection( new dot.Ray2( p( 0, 1 ), p( 1, 0 ) ) ), 1 );
+    equal( line.windingIntersection( new dot.Ray2( p( 0, 5 ), p( 1, 0 ) ) ), 0 );
+    equal( line.windingIntersection( new dot.Ray2( p( 1, 0 ), p( 0, 1 ) ) ), -1 );
+    equal( line.windingIntersection( new dot.Ray2( p( 0, 0 ), p( 1, 1 ).normalized() ) ), 0 );
+    equal( line.windingIntersection( new dot.Ray2( p( 0, 1 ), p( 1, 1 ).normalized() ) ), 0 );
+  } );
+  
+  test( 'Rectangle hit testing', function() {
+    var shape = Shape.rectangle( 0, 0, 1, 1 );
+    
+    equal( shape.containsPoint( p( 0.2, 0.3 ) ), true, '0.2, 0.3' );
+    equal( shape.containsPoint( p( 0.5, 0.5 ) ), true, '0.5, 0.5' );
+    equal( shape.containsPoint( p( 1.5, 0.5 ) ), false, '1.5, 0.5' );
+    equal( shape.containsPoint( p( -0.5, 0.5 ) ), false, '-0.5, 0.5' );
+  } );
+  
+  test( 'Un-closed shape hit testing', function() {
+    var shape = new Shape().moveTo( 0, 0 ).lineTo( 10, 10 ).lineTo( 0, 10 );
+    
+    equal( shape.containsPoint( p( 1, 2 ) ), true, '1, 2' );
+    equal( shape.containsPoint( p( 10, 2 ) ), false, '10, 2' );
+  } );
+  
+})();
---
+++ tests/qunit/unit-tests.js
@@ -0,0 +1,16 @@
+
+// path to test base, without a slash, e.g. '../../common/scenery/tests/qunit'
+function runKiteTests( pathToTestBase ) {
+  function loadTestFile( src ) {
+    var script = document.createElement( 'script' );
+    script.type = 'text/javascript';
+    script.async = false;
+    
+    // make sure things aren't cached, just in case
+    script.src = pathToTestBase + '/' + src + '?random=' + Math.random().toFixed( 10 );
+    
+    document.getElementsByTagName( 'head' )[0].appendChild( script );
+  }
+  
+  loadTestFile( 'js/miscellaneous.js' );
+};
