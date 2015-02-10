2c6cfed19197b041abcd19c1cdc893ffbb5d579f
Jonathan Olson <olsonsjc@gmail.com>
2015-02-09 20:46:42 -0700

    Reverting config cache busters on common libs that don't depend on phetcommon (see https://github.com/phetsims/scenery/issues/380), should be handled properly after the code is moved to chipper (https://github.com/phetsims/phetcommon/issues/23)

--- js/config.js
+++ js/config.js
@@ -16,5 +16,5 @@ require.config( {
   },
 
   // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
-  urlArgs: phet.phetcommon.getCacheBusterArgs()
+  urlArgs: Date.now()
 } );
