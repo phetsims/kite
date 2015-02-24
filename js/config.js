1d3668fa88ee160dd17bbf13a03edc0389938b7d
Jonathan Olson <olsonsjc@gmail.com>
2015-02-23 22:14:52 -0700

    Removing unnecessary jquery/underscore shims in config files

--- js/config.js
+++ js/config.js
@@ -4,15 +4,10 @@ require.config( {
   deps: [ 'main', 'DOT/main', 'PHET_CORE/main' ],
 
   paths: {
-    underscore: '../../sherpa/lodash-2.4.1',
     KITE: '.',
     DOT: '../../dot/js',
     PHET_CORE: '../../phet-core/js',
     AXON: '../../axon/js'
-  },
-
-  shim: {
-    underscore: { exports: '_' }
   },
 
   // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
