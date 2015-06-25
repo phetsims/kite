6a46e847321af03e39f8b5df1ef4024ea9d5b591
Jonathan Olson <olsonsjc@gmail.com>
2015-06-25 10:35:11 -0600

    Documentation for https://github.com/phetsims/kite/issues/50

--- js/kite.js
+++ js/kite.js
@@ -17,6 +17,10 @@ define( function( require ) {
   window.phetAllocation = require( 'PHET_CORE/phetAllocation' );
 
   var kite = {
+    // Since SVG doesn't support parsing scientific notation (e.g. 7e5), we need to output fixed decimal-point strings.
+    // Since this needs to be done quickly, and we don't particularly care about slight rounding differences (it's
+    // being used for display purposes only, and is never shown to the user), we use the built-in JS toFixed instead of
+    // Dot's version of toFixed. See https://github.com/phetsims/kite/issues/50
     svgNumber: function( n ) {
       return n.toFixed( 20 );
     }
