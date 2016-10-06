480fe0be456087779ebd50271d4fa85f4263d101
Jonathan Olson <olsonsjc@gmail.com>
2016-10-06 01:15:47 -0600

    Fixing paths, adding notes about building (for tests and build-js change). See https://github.com/phetsims/kite/issues/63

--- js/kite-main.js
+++ js/kite-main.js
@@ -4,9 +4,6 @@
   if ( !window.hasOwnProperty( '_' ) ) {
     throw new Error( 'Underscore/Lodash not found: _' );
   }
-  if ( !window.hasOwnProperty( '$' ) ) {
-    throw new Error( 'jQuery not found: $' );
-  }
   define( function( require ) {
 
     window.kite = require( 'KITE/main' );
