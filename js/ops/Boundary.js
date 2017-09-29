937720f5c88e9d232453db02a00a9b3e489710a2
Jonathan Olson <olsonsjc@gmail.com>
2017-09-29 15:12:53 -0600

    Boundary cleanup

--- js/ops/Boundary.js
+++ js/ops/Boundary.js
@@ -1,7 +1,8 @@
 // Copyright 2017, University of Colorado Boulder
 
 /**
- * TODO: doc
+ * A boundary is a loop of directed half-edges that always follow in the tightest counter-clockwise direction around
+ * vertices.
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
@@ -51,18 +52,36 @@ define( function( require ) {
       this.childBoundaries = cleanArray( this.childBoundaries );
     },
 
+    /**
+     * Removes references (so it can allow other objects to be GC'ed or pooled), and frees itself to the pool so it
+     * can be reused.
+     * @public
+     */
     dispose: function() {
       this.halfEdges = [];
       cleanArray( this.childBoundaries );
       this.freeToPool();
     },
 
+    /**
+     * Returns whether this boundary is essentially "counter-clockwise" (in the non-reversed coordinate system) with
+     * positive signed area, or "clockwise" with negative signed area.
+     * @public
+     *
+     * Boundaries are treated as "inner" boundaries when they are counter-clockwise, as the path followed will generally
+     * follow the inside of a face (given how the "next" edge of a vertex is computed).
+     *
+     * @returns {number}
+     */
     isInner: function() {
       return this.signedArea > 0;
     },
 
     /**
+     * Returns the signed area of this boundary, given its half edges.
      * @public
+     *
+     * Each half-edge has its own contribution to the signed area, which are summed together.
      *
      * @returns {number}
      */
@@ -75,6 +94,7 @@ define( function( require ) {
     },
 
     /**
+     * Returns the bounds of the boundary (the union of each of the boundary's segments' bounds).
      * @public
      *
      * @returns {Bounds2}
@@ -88,12 +108,28 @@ define( function( require ) {
       return bounds;
     },
 
+    /**
+     * Returns a point on the boundary which, when the shape (and point) are transformed with the given transform, would
+     * be a point with the minimal y value.
+     * @public
+     *
+     * Will only return one point, even if there are multiple points that have the same minimal y values for the
+     * boundary. The point may be at the end of one of the edges/segments (at a vertex), but also may somewhere in the
+     * middle of an edge/segment.
+     *
+     * @param {Transform3} transform - Transform used because we want the inverse also.
+     * @returns {Vector2}
+     */
     computeExtremePoint: function( transform ) {
+      assert && assert( this.halfEdges.length > 0, 'There is no extreme point if we have no edges' );
+
+      // Transform all of the segments into the new transformed coordinate space.
       var transformedSegments = [];
       for ( var i = 0; i < this.halfEdges.length; i++ ) {
         transformedSegments.push( this.halfEdges[ i ].edge.segment.transformed( transform.getMatrix() ) );
       }
 
+      // Find the bounds of the entire transformed boundary
       var transformedBounds = Bounds2.NOTHING.copy();
       for ( i = 0; i < transformedSegments.length; i++ ) {
         transformedBounds.includeBounds( transformedSegments[ i ].getBounds() );
@@ -101,50 +137,62 @@ define( function( require ) {
 
       for ( i = 0; i < transformedSegments.length; i++ ) {
         var segment = transformedSegments[ i ];
+
+        // See if this is one of our potential segments whose bounds have the minimal y value. This indicates at least
+        // one point on this segment will be a minimal-y point.
         if ( segment.getBounds().top === transformedBounds.top ) {
-          var extremePoint = new Vector2( 0, Number.POSITIVE_INFINITY );
+          // Pick a point with values that guarantees any point will have a smaller y value.
+          var minimalPoint = new Vector2( 0, Number.POSITIVE_INFINITY );
+
+          // Grab parametric t-values for where our segment has extreme points, and adds the end points (which are
+          // candidates). One of the points at these values should be our minimal point.
           var tValues = [ 0, 1, ].concat( segment.getInteriorExtremaTs() );
           for ( var j = 0; j < tValues.length; j++ ) {
             var point = segment.positionAt( tValues[ j ] );
-            if ( point.y < extremePoint.y ) {
-              extremePoint = point;
+            if ( point.y < minimalPoint.y ) {
+              minimalPoint = point;
             }
           }
-          return transform.inversePosition2( extremePoint );
+
+          // Transform this minimal point back into our (non-transformed) boundary's coordinate space.
+          return transform.inversePosition2( minimalPoint );
         }
       }
 
-      throw new Error( 'No... segments?' );
+      throw new Error( 'Should not reach here if we have segments' );
     },
 
+    /**
+     * Returns a ray (position and direction) pointing away from our boundary at an "extreme" point, so that the ray
+     * will be guaranteed not to intersect this boundary.
+     * @public
+     *
+     * The ray's position will be slightly offset from the boundary, so that it will not technically intersect the
+     * boundary where the extreme point lies. The extreme point will be chosen such that it would have the smallest
+     * y value when the boundary is transformed by the given transformation.
+     *
+     * The ray's direction will be such that if the ray is transformed by the given transform, it will be pointing
+     * in the negative-y direction (e.g. a vector of (0,-1)). This should guarantee it is facing away from the
+     * boundary, and will be consistent in direction with other extreme rays (needed for its use case with the
+     * boundary graph).
+     *
+     * @param {Transform3} transform
+     * @returns {Ray2}
+     */
     computeExtremeRay: function( transform ) {
       var extremePoint = this.computeExtremePoint( transform );
       var orientation = transform.inverseDelta2( new Vector2( 0, -1 ) ).normalized();
+      // TODO: gather this epsilon somewhere?
       return new Ray2( extremePoint.plus( orientation.timesScalar( 1e-4 ) ), orientation );
     },
 
     /**
+     * Returns whether this boundary includes the specified half-edge.
      * @public
      *
-     * @returns {Vector2}
+     * @param {HalfEdge} halfEdge
+     * @returns {boolean}
      */
-    computeMinYPoint: function() {
-      for ( var i = 0; i < this.halfEdges.length; i++ ) {
-        var segment = this.halfEdges[ i ].edge.segment;
-        if ( segment.getBounds().top === this.bounds.top ) {
-          var minYPoint = new Vector2( 0, Number.POSITIVE_INFINITY );
-          var tValues = [ 0, 1, ].concat( segment.getInteriorExtremaTs() );
-          for ( var j = 0; j < tValues.length; j++ ) {
-            var point = segment.positionAt( tValues[ j ] );
-            if ( point.y < minYPoint.y ) {
-              minYPoint = point;
-            }
-          }
-          return minYPoint;
-        }
-      }
-    },
-
     hasHalfEdge: function( halfEdge ) {
       for ( var i = 0; i < this.halfEdges.length; i++ ) {
         if ( this.halfEdges[ i ] === halfEdge ) {
@@ -154,6 +202,12 @@ define( function( require ) {
       return false;
     },
 
+    /**
+     * Converts this boundary to a Subpath, so that we can construct things like Shape objects from it.
+     * @public
+     *
+     * @returns {Subpath}
+     */
     toSubpath: function() {
       var segments = [];
       for ( var i = 0; i < this.halfEdges.length; i++ ) {
