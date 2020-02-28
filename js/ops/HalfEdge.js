12cb890a1937e047121962c389b88b7e0a6917e7
phet-dev <phetadmin@gmail.com>
2020-02-28 03:32:46 -0700

    update copyright dates from daily grunt work

--- js/ops/HalfEdge.js
+++ js/ops/HalfEdge.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2019, University of Colorado Boulder
+// Copyright 2017-2020, University of Colorado Boulder
 
 /**
  * Represents a single direction/side of an Edge. There are two half-edges for each edge, representing each direction.
