7010510fa640e79f451912910c4be6b51436d1d4
zepumph <michael.kauzmann@colorado.edu>
2020-05-21 13:19:22 -0800

    convert all tests to use qunitStart.js, https://github.com/phetsims/utterance-queue/issues/12#event-3334154840

--- js/kite-tests.js
+++ js/kite-tests.js
@@ -7,6 +7,7 @@
  */
 
 import './ShapeTests.js';
+import qunitStart from '../../chipper/js/sim-tests/qunitStart.js';
 
 // Since our tests are loaded asynchronously, we must direct QUnit to begin the tests
-QUnit.start();
+qunitStart();
