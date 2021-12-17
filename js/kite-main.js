94e901a5ad9dfeac064fbff98fa9c3f8c3af070e
Jesse <jesse.greenberg@colorado.edu>
2021-12-16 19:14:32 -0500

    disable default-import-match-filename in kite-main.js, see https://github.com/phetsims/chipper/issues/1177

--- js/kite-main.js
+++ js/kite-main.js
@@ -1,9 +1,9 @@
-import axon from '../../axon/js/main.js';
-import dot from '../../dot/js/main.js';
-import phetCore from '../../phet-core/js/main.js';
-import kite from './main.js';
+// Copyright 2016-2021, University of Colorado Boulder
 
-// Copyright 2016-2019, University of Colorado Boulder
+import axon from '../../axon/js/main.js'; // eslint-disable-line default-import-match-filename
+import dot from '../../dot/js/main.js'; // eslint-disable-line default-import-match-filename
+import phetCore from '../../phet-core/js/main.js'; // eslint-disable-line default-import-match-filename
+import kite from './main.js'; // eslint-disable-line default-import-match-filename
 
 if ( !window.hasOwnProperty( '_' ) ) {
   throw new Error( 'Underscore/Lodash not found: _' );
