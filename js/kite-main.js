1dc3ba60dfd968f5e2554e4d211dd4f46bdf2075
Jonathan Olson <jonathan.olson@colorado.edu>
2023-02-23 16:31:58 -0700

    Moving exclusively to phet.X namespaces, see https://github.com/phetsims/scenery/issues/966

--- js/kite-main.js
+++ js/kite-main.js
@@ -1,15 +1,10 @@
 // Copyright 2016-2022, University of Colorado Boulder
 
-import axon from '../../axon/js/main.js'; // eslint-disable-line default-import-match-filename
-import dot from '../../dot/js/main.js'; // eslint-disable-line default-import-match-filename
-import phetCore from '../../phet-core/js/main.js'; // eslint-disable-line default-import-match-filename
-import kite from './main.js'; // eslint-disable-line default-import-match-filename
+import '../../axon/js/main.js';
+import '../../dot/js/main.js';
+import '../../phet-core/js/main.js';
+import './main.js';
 
 if ( !window.hasOwnProperty( '_' ) ) {
   throw new Error( 'Underscore/Lodash not found: _' );
 }
-
-window.axon = axon;
-window.dot = dot;
-window.kite = kite;
-window.phetCore = phetCore;
