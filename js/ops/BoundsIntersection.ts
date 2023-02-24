7f468920233030bff0317a3de3cf31bc5df23dc1
phet-dev <phetadmin@gmail.com>
2023-02-24 04:20:08 -0700

    update copyright dates from daily grunt work

--- js/ops/BoundsIntersection.ts
+++ js/ops/BoundsIntersection.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2022, University of Colorado Boulder
+// Copyright 2017-2023, University of Colorado Boulder
 
 /**
  * A region of two segments that intersects (contains static functions for segment intersection).
