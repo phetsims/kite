671992a1d0e2538bd1a68a595973d409633f0dd1
phet-dev <phetadmin@gmail.com>
2023-04-08 05:32:30 -0600

    update copyright dates from daily grunt work

--- js/segments/Line.ts
+++ js/segments/Line.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2022, University of Colorado Boulder
+// Copyright 2013-2023, University of Colorado Boulder
 
 /**
  * A line segment (all points directly between the start and end point)
