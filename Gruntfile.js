f5887e66eff3a85c8f95f952c404c6add89b90b0
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-10-14 16:00:48 -0600

    better pattern, https://github.com/phetsims/chipper/issues/1464

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,13 @@
 // Copyright 2013-2016, University of Colorado Boulder
 
-
-// use chipper's gruntfile
-const Gruntfile = require( '../chipper/js/grunt/gruntMain.js' ); // eslint-disable-line phet/require-statement-match
+const Gruntfile = require( '../chipper/Gruntfile' );
 const registerTasks = require( '../perennial-alias/js/grunt/util/registerTasks' );
 
-// Add repo-specific grunt tasks
+/**
+ * Kite grunt tasks
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
 module.exports = function( grunt ) {
+  Gruntfile( grunt ); // use chipper's gruntfile
   registerTasks( grunt, `${__dirname}/js/grunt/tasks/` );
-
-  Gruntfile( grunt );
 };
